c874243c3cf055de836527490b8ac3494daa28c5
Allen Shen <t-yoush@microsoft.com>
2016-12-06 15:31:52 -0800

    added FastCore

---
+++ GenArt.FastCore/Models.fs
@@ -0,0 +1,43 @@
+﻿namespace GenArt.FastCore.Models
+
+type Brush =
+    struct
+        val R: int
+        val G: int
+        val B: int
+        val Alpha: int
+
+        new (r, g, b, alpha) =
+            { R = r; G = g; B = b; Alpha = alpha; }
+    end
+
+type Color =
+    struct
+        val R: int
+        val G: int
+        val B: int
+
+        new (r, g, b) =
+            { R = r; G = g; B = b; }
+    end
+
+type Point =
+    struct
+        val X: int
+        val Y: int
+        val Color: Color
+
+        new (x, y, color) =
+            { X = x; Y = y; Color = color; }
+    end
+
+type Image = {
+    Width: int
+    Height: int
+    Points: Point array
+}
+
+type Polygon = {
+    Points: Point array
+    Brush: Brush
+}
