17ab3d263a6bf228259a0fcd3014b0d6ddbd21aa
Allen Shen <t-yoush@microsoft.com>
2016-12-11 12:00:06 -0800

    Refactor

--- GenArt.FastCore/Program.fs
+++ GenArt.FastCore/Program.fs
@@ -9,11 +9,4 @@ let main args =
     let bitmap = ReadBitmapFromFile @"C:\Users\t-yoush.REDMOND\Desktop\darwin.jpg"
     let img = ConvertBitmapToDnaImage bitmap
     let mutable error = 0
-    let watch = System.Diagnostics.Stopwatch.StartNew()
-    watch.Start
-    for i = 0 to 1000 do
-        let img2 = img.Mutate
-        error <- error + DnaImage.CalculateFitness(img, img2)
-    watch.Stop
-    printfn "%A" watch.ElapsedMilliseconds
     0
